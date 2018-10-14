65c38028ba3b79ea7c92f2137bebb8acf6e3b51e
subramanianmenakshisundaram <00subra8@gmail.com>
2018-10-14 05:22:45 +0100

    Initial commit...

---
+++ src/main/java/com/ig/eval/util/CoffeeHouseUtil.java
@@ -0,0 +1,4 @@
+package com.ig.eval.util;
+
+public class CoffeeHouseUtil {
+}
---
+++ src/test/java/com/ig/eval/util/CoffeeHouseUtilSpec.groovy
@@ -0,0 +1,11 @@
+package com.ig.eval.util
+
+import spock.lang.Specification
+
+class CoffeeHouseUtilSpec extends Specification {
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+}
